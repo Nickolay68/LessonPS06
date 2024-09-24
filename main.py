d1199dd8cd70a7bbbffd3a43bd6f55525d687917
Nickolay68/LessonOB02 <p.nick21vek@gmail.com>
2024-09-24 18:43:24 +0300

    finPS

--- main.py
+++ main.py
@@ -11,12 +11,24 @@ driver.get(url)
 
 time.sleep(5)
 
-sale = driver.find_element(By.CSS_SELECTOR, 'div.class=mk_t7')
+sale = driver.find_elements(By.CLASS_NAME, 'div.yo4lA wcwtp')
 
 parsed_data = []
 
 for sale in sale:
     try:
-        title = sale.find_element(By.CSS_SELECTOR, 'div.VyQMt')
-        furniture = sale.find_element(By.CSS_SELECTOR, 'Pk6w8 fJEzt')
-        priсe = sale.find_element(By.CSS_SELECTOR, 'ui-LD-ZU rCNq4')
+        title = sale.find_element(By.CSS_SELECTOR, 'div.page-category').text
+        furniture = sale.find_element(By.CSS_SELECTOR, 'div.NuViy S5rWI').text
+        priсe = sale.find_element(By.CSS_SELECTOR, 'span.class="ui-An69V"').text
+    except:
+        print("произошла ошибка при парсинге")
+        continue
+
+        parsed_data.append([title, furniture, price])
+
+driver.quit()
+
+with open("hh.csv", 'w', newline='',  encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['название, наборы мебели, цена'])
+    writer.writerows(parsed_data)
